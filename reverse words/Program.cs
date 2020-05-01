1cb4c44fd9a24833fcb31cc604ebdac4880b85fb
unset <amrmedhat339@gmail.com>
2020-05-02 00:51:38 +0200

    first commit

---
+++ reverse words/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace reverse_words
+{
+    class Program
+    {
+        //reversed words
+        public static void reversed(string before)
+        {
+            string after = "";
+            int x = before.Length - 1;
+            while (x >= 0)
+            {
+                after = after + before[x];
+                x--;
+            }
+            Console.WriteLine("the string before reversed is: " + before + " \nthe string after reversed is: " + after);
+        }
+        static void Main(string[] args)
+        {
+            //reversed words
+            string n = "amr";
+            reversed(n);
+            Console.ReadKey();
+        }
+    }
+}
